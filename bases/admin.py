681cb0b1531780f5cde2a0baecb4134b6ecf99ce
daniellcobos <daniel.leocobos@gmail.com>
2021-06-03 20:37:30 -0500

    todoooo

--- bases/admin.py
+++ bases/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Avaluador, Examen
+
+class AvaluadorAdmin(admin.ModelAdmin):
+    search_fields = ('RNA',"Apellidos" )
+
 # Register your models here.
-admin.site.register(Avaluador)
+admin.site.register(Avaluador,AvaluadorAdmin)
 admin.site.register(Examen)
+
--- bases/models.py
+++ bases/models.py
@@ -2,27 +2,38 @@ from django.db import models
 
 # Create your models here.
 class Avaluador(models.Model):
-    RNA = models.BigIntegerField(primary_key=True)
-    CC = models.BigIntegerField(unique=True)
+    RNA = models.BigIntegerField(primary_key=True,verbose_name = "Codigo RNA (Obligatorio)")
+    Identificacion = models.BigIntegerField(unique=True)
     Nombre = models.CharField(max_length=100)
-    Year = models.SmallIntegerField()
-    Email1=models.EmailField()
+    Apellidos = models.CharField(max_length=100)
+    Year = models.DateField(verbose_name = "Fecha de Nacimiento")
+    Email1=models.EmailField(verbose_name = "Email (Obligatorio)")
     Email2=models.EmailField(blank=True)
     Telefono=models.CharField(blank=True,max_length=100)
     Fax= models.CharField(blank=True,max_length=100)
-    Celular=models.CharField(blank=True,max_length=100)
+    Celular=models.CharField(max_length=100, verbose_name = "Telefono Movil (Obligatorio)")
     Direccion = models.CharField(blank=True,max_length=100)
     Ciudad = models.CharField(blank=True,max_length=100)
     ConReg= models.CharField(blank=True,max_length=100)
     Afliado = models.BooleanField()
+    Fallecido = models.BooleanField()
+    Suspendido = models.BooleanField()
     Comentarios = models.TextField(blank=True)
+    Codinter = models.CharField(blank=True,max_length=100, verbose_name = "Codigo Internacional")
+    Pais = models.CharField(blank=True,max_length=50)
+    def __str__(self):
+        return str(self.RNA)
     class Meta:
         verbose_name_plural = "Avaluadores"
 
 class Examen(models.Model):
     Categoria= models.CharField(blank=True,max_length=100)
     Codigo = models.BigIntegerField(primary_key=True)
+    RNA = models.ForeignKey(Avaluador,on_delete=models.CASCADE)
+    Solicitud = models.DateField()
+    Aprobacion = models.DateField()
     Otorgamiento = models.DateField()
+    PrimerVencimiento = models.DateField()
     Renovacion = models.DateField()
     Vencimiento = models.DateField()
     class Meta:
