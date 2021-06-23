a7d96e8b575d5939026dfade3d31e7c34b908e75
unknown <daniel.leocobos@gmail.com>
2021-06-23 13:21:06 -0500

    ajustes menores a peticion del publico

--- bases/admin.py
+++ bases/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from .models import Avaluador, Examen
-
+class Exameninline(admin.TabularInline):
+    model = Examen
 class AvaluadorAdmin(admin.ModelAdmin):
     search_fields = ('RNA',"Apellidos" )
+    inlines = [Exameninline]
 class ExamenAdmin(admin.ModelAdmin):
-    search_fields = ["Codigo"]
+    search_fields = ["Codigo",'RNA__RNA']
+
 # Register your models here.
 admin.site.register(Avaluador,AvaluadorAdmin)
 admin.site.register(Examen,ExamenAdmin)
--- bases/views.py
+++ bases/views.py
@@ -23,33 +23,38 @@ def WriteToExcel(weather_data, town=None):
     'valign': 'top',
     'border': 1})
     center = workbook.add_format({'align': 'center','valign': 'center',})
-    worksheet.set_column('A:E',15)
-    worksheet.set_column('F:G',25)
-    worksheet.set_column('H:CC',20)
+    worksheet.set_column('A:E',20)
+    worksheet.set_column('F:G',30)
+    worksheet.set_column('H:CC',25)
     query = Avaluador.objects.values()
-    eheader = Examen.objects.values().first().keys()
-    heads = query.first().keys()
-    
-    columna_inicial = 19
+    eheader = list(Examen.objects.values().first().keys())
+    heads = list(query.first().keys())
+    heads = heads[:-3]
+   
+    columna_inicial = 17
     for n in range(6):
         for i, e in enumerate(eheader):
             worksheet.write(0, columna_inicial, e,headere)
             columna_inicial = columna_inicial + 1
+    worksheet.write(0, 16, 'Estado',headerf)
     for i,d in enumerate(heads):
         worksheet.write(0, i, d,headerf)
     for idx, data in enumerate(query):
+        estado = ''
         row = 1 + idx
         ldata = list(data.values())
-        for i in range(len(data)):
+        for i in range(len(data)-3):
             write = ldata[i]
-            
-            if write == False:
-               write = "No"
-            if  write == True:
-                write = "Si"
             worksheet.write(row,i,write,center)
+        if ldata[16]:
+            estado = estado + 'Afiliado '
+        if ldata[17]:
+            estado = estado + 'Fallecido '
+        if ldata[18]:
+            estado = estado + 'Suspendido '
+        worksheet.write(row,16,estado,center)
         query2= Examen.objects.filter(RNA = data['RNA']).values()
-        columns = 19
+        columns = 17
         for q in query2:
             listq = list(q.values())
             for e in listq:
