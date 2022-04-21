f8b7f2918b71ddb2419574eeda6702c8a66e29af
daniel.leocobos@gmail.com <daniel.leocobos@gmail.com>
2022-04-21 10:06:59 -0500

    test cors

--- rna/settings.py
+++ rna/settings.py
@@ -57,9 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
 ROOT_URLCONF = 'rna.urls'
 
 TEMPLATES = [
